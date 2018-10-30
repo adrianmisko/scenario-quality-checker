0a3b289242094d8f7e2ddcb1e33e3ec0c0b9bbde
adrian <adrian.misko5@gmail.com>
2018-10-30 14:29:21 +0100

    removed unnecessary import

--- src/main/java/pl/put/poznan/qualitychecker/rest/ScenarioQualityCheckerController.java
+++ src/main/java/pl/put/poznan/qualitychecker/rest/ScenarioQualityCheckerController.java
@@ -3,7 +3,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 import pl.put.poznan.qualitychecker.logic.ScenarioQualityChecker;
-import pl.put.poznan.qualitychecker.models.Scenario;
 
 import java.util.Arrays;
 
