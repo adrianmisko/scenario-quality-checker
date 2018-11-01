67362ddf76b1ef21cff19f1214feadd5ce0440ee
adrian <adrian.misko5@gmail.com>
2018-11-01 15:37:51 +0100

    fixed pom.xml

--- src/main/java/pl/put/poznan/qualitychecker/rest/ScenarioQualityCheckerController.java
+++ src/main/java/pl/put/poznan/qualitychecker/rest/ScenarioQualityCheckerController.java
@@ -1,8 +1,6 @@
 package pl.put.poznan.qualitychecker.rest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import pl.put.poznan.qualitychecker.logic.ScenarioQualityChecker;
 import pl.put.poznan.qualitychecker.models.Scenario;
