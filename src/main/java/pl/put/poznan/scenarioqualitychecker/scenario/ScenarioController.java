b17f79847f65138877dcc7c3ce07273d0058e683
adrian <adrian.misko5@gmail.com>
2018-11-03 12:08:50 +0100

    get reqest params are no longer required added primitive reponse entities

--- src/main/java/pl/put/poznan/scenarioqualitychecker/scenario/ScenarioController.java
+++ src/main/java/pl/put/poznan/scenarioqualitychecker/scenario/ScenarioController.java
@@ -2,6 +2,8 @@ package pl.put.poznan.scenarioqualitychecker.scenario;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import pl.put.poznan.scenarioqualitychecker.scenario.dao.Scenario;
 
@@ -15,22 +17,17 @@ public class ScenarioController {
 
     @RequestMapping(value = "/scenario/{id}", method = RequestMethod.GET,
             consumes = "application/json", produces = "application/json")
-    public String getScenario(@PathVariable long id, @RequestParam(value = "api") String[] params) {
+    public ResponseEntity getScenario(@PathVariable long id, @RequestParam(value = "api", required = false) String[] params) {
 
-        return Long.toString(id);
-
+        return new ResponseEntity("{ \"error\" : \"not implemented yet\" }", HttpStatus.NOT_IMPLEMENTED);
     }
 
     @RequestMapping(value = "/scenario", method = RequestMethod.POST,
             consumes = "application/json", produces = "application/json")
-    public String addScenario(@RequestBody @Valid Scenario scenario) {
+    public ResponseEntity addScenario(@RequestBody @Valid Scenario scenario) {
 
         service.createScenario(scenario);
-
-        for (Scenario scenario1 : service.getAllScenarios()) {
-            System.out.println(scenario1.getId());
-        }
-        return Long.toString(scenario.getId());
+        return new ResponseEntity("{ \"message\" : \"success\", \"id\" : \"" + Long.toString(scenario.getId()) +"\" }", HttpStatus.CREATED);
 
     }
 
