966f2eb8b67ee6c6163486cecf00af997f03867e
adrianmisko <33435918+adrianmisko@users.noreply.github.com>
2018-11-13 15:28:10 +0100

    Update push-javadoc.sh

--- .travis/push-javadoc.sh
+++ .travis/push-javadoc.sh
@@ -4,11 +4,10 @@ if [    "$TRAVIS_REPO_SLUG" = "adrianmisko/scenario-quality-checker" -a \
         "$TRAVIS_PULL_REQUEST" = "false" -a \
         "$TRAVIS_BRANCH" = "master" ]
 then
-  
-  echo "$HOME"
+
   echo "Publishing javadoc..."
 
-  cp -R target/site/apidocs docs
+  cp -R target/site/apidocs $HOME/apidocs
   
   cd $HOME
   git config --global user.email "travis@travis-ci.org"
@@ -18,7 +17,7 @@ then
   cd gh-pages
   git rm -rf ./docs
   mkdir -p ./docs
-  cp -Rf docs ./scenario-quality-checker
+  cp -Rf $HOME/apidocs ./docs
   git add -f .
   git commit -m "Latest javadoc on successful travis build $TRAVIS_BUILD_NUMBER auto-pushed [skip ci]"
   git push -fq origin master > /dev/null
