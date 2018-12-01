0c1450baa6726b1afe124ac412f01b66540a7405
trzcioli <trzcioli@gmail.com>
2018-12-02 00:28:02 +0100

    add "Download the numbered list of steps" feature

---
+++ src/main/java/pl/put/poznan/scenarioqualitychecker/scenario/visitors/NumberedListOfSteps.java
@@ -0,0 +1,53 @@
+package pl.put.poznan.scenarioqualitychecker.scenario.visitors;
+
+import com.google.common.base.Strings;
+import lombok.Getter;
+import lombok.Setter;
+import pl.put.poznan.scenarioqualitychecker.scenario.Visitor;
+import pl.put.poznan.scenarioqualitychecker.scenario.model.Scenario;
+import pl.put.poznan.scenarioqualitychecker.scenario.model.Step;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class NumberedListOfSteps implements Visitor{
+
+    @Getter
+    @Setter
+    private List<String> numberedList;
+    private int numOfScenarios;
+    private int numOfSteps;
+    private Stack<Integer> stackOfSteps;
+
+    public NumberedListOfSteps() {
+        numOfScenarios = 0;
+        numOfSteps = 0;
+        numberedList = new ArrayList<>();
+        stackOfSteps = new Stack<>();
+    }
+
+    @Override
+    public void visit(Step s) {
+        numOfSteps++;
+        numberedList.add(Strings.repeat("  ", numOfScenarios-1) + Integer.toString(numOfScenarios) + '.' + Integer.toString(numOfSteps) +  ". " + s.getText());
+    }
+
+    @Override
+    public void visit(Scenario scenario) {
+        numOfScenarios++;
+        stackOfSteps.push(numOfSteps);
+        numOfSteps = 0;
+    }
+
+    @Override
+    public void afterVisit(Step step) {
+
+    }
+
+    @Override
+    public void afterVisit(Scenario scenario) {
+        numOfScenarios--;
+        numOfSteps = stackOfSteps.pop();
+    }
+}
