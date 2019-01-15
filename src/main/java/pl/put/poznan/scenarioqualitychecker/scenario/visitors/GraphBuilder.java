72251c88fe4c950c80cec52c812bcbb1abf72929
adrian <adrian.misko5@gmail.com>
2019-01-15 19:11:26 +0100

    add graph builder

--- src/main/java/pl/put/poznan/scenarioqualitychecker/scenario/visitors/GraphBuilder.java
+++ src/main/java/pl/put/poznan/scenarioqualitychecker/scenario/visitors/GraphBuilder.java
@@ -1,13 +1,14 @@
 package pl.put.poznan.scenarioqualitychecker.scenario.visitors;
 
-import lombok.Getter;
-import lombok.Setter;
+
 import pl.put.poznan.scenarioqualitychecker.scenario.Visitor;
 import pl.put.poznan.scenarioqualitychecker.scenario.model.Scenario;
 import pl.put.poznan.scenarioqualitychecker.scenario.model.Step;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
+
 
 /**
  * This class builds description of graph in a simple text language, to be consumed by graphviz software,
@@ -21,6 +22,9 @@ public class GraphBuilder implements Visitor {
     private StringBuilder sb = new StringBuilder();
     private int currentDepth = -1;
     private int currentStep = 0;
+    private Step lastStep;
+    private List<Integer> stepsLength = new ArrayList<>();
+    private Stack<Integer> stepNumbers = new Stack<>();
 
     public GraphBuilder() {
         sb.append("strict digraph {\n");
@@ -28,35 +32,54 @@ public class GraphBuilder implements Visitor {
 
     @Override
     public void visit(Step step) {
-        if (currentStep % 2 == 0) {
-            sb.append("\t\"");
-            sb.append(step.getText());
-            sb.append("\"->");
-        } else {
-            sb.append(step.getText());
-            sb.append("[rank=\"");
-            sb.append(currentDepth);
-            sb.append("\"]\n");
-        }
+            if (currentStep == 0) {
+                sb.append("\t\"");
+                sb.append(step.getText());
+                sb.append("\" -> ");
+            } else {
+                sb.append("\"");
+                sb.append(step.getText());
+                sb.append("\" [rank=");
+                sb.append(currentDepth);
+                sb.append("]\n\t");
+                if (stepsLength.get(currentDepth) != currentStep + 1) {
+                    sb.append("\"");
+                    sb.append(step.getText());
+                    sb.append("\" -> ");
+                }
+            }
+        currentStep++;
+        lastStep = step;
     }
 
     @Override
     public void visit(Scenario scenario) {
         currentDepth++;
+        if (currentDepth > 0) {
+            sb.append("\"");
+            sb.append(lastStep.getText());
+            sb.append("\" -> ");
+        }
+        stepsLength.add(scenario.getSteps().size());
+        stepNumbers.push(currentStep);
+        currentStep = 0;
     }
 
     @Override
     public void afterVisit(Step step) {
-        currentStep++;
     }
 
     @Override
     public void afterVisit(Scenario scenario) {
+        sb.append("\"");
+        sb.append(lastStep.getText());
+        sb.append("\" -> ");
         currentDepth--;
+        currentStep = stepNumbers.pop();
     }
 
     public void onFinish() {
-        sb.append("\n}");
+        sb.append("}");
     }
 
     public String getScenarioGraphInTextForm() {
